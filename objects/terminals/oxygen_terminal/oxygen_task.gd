5bc0c806302a3d036b672643c971b52cb7cb93ff
Domara Shlimon <domara2000@icloud.com>
2026-07-25 18:09:03 -0400

    oxygen task visuals done

--- objects/terminals/oxygen_terminal/oxygen_task.gd
+++ objects/terminals/oxygen_terminal/oxygen_task.gd
@@ -1,38 +1,38 @@
 class_name OxygenTask
-extends Node2D
-
-
-## Public variables
-@export var red_bar: Area2D
-@export var green_zone: Area2D
-@export var move_speed: float
-
-@export var button: InteractableButton
-
-@export var timer_initial: float
-
-@export var stat_success_amount: float = 0.0
-
-@export_custom(PROPERTY_HINT_NONE, "suffix:/s")
-var stat_usage_rate: float = 1.0
-
-## Private variables
-@export var _upper_limits: float
-@export var _lower_limits: float
-
-var _is_moving_down: bool = true
-var _can_move: bool = true
-var _is_colliding: bool = false
-
-@export var _success_boxes: Array[Sprite2D] = []
-var _successes: int = 0
-
-var _timer: float
+extends Control
 
 ## Signals
 signal on_oxygen_terminal_success()
 signal on_oxygen_terminal_completion()
 signal on_oxygen_terminal_failure()
+
+
+## Public variables
+@export var button: InteractableButton
+@export var red_bar: Control
+@export var green_zone: Control
+@export var _success_boxes: Array[ColorRect] = []
+@export var arrows: Array[TextureRect] = []
+
+@export var move_speed: float
+@export var timer_initial: float
+@export var stat_success_amount: float = 0.0
+@export_custom(PROPERTY_HINT_NONE, "suffix:/s")
+var stat_usage_rate: float = 1.0
+@export var _green_lower_limit: float
+@export var _red_lower_limit: float
+
+@export var green: Color
+@export var red: Color
+
+## Private variables
+var _is_moving_down: bool = true
+var _can_move: bool = true
+var _is_colliding: bool = false
+var _successes: int = 0
+var _timer: float
+var _should_be_moving: bool = true
+
 
 ## Private functions
 func _ready() -> void:
@@ -56,6 +56,8 @@ func _process(delta: float) -> void:
 
 
 func _move_red_bar(delta: float) -> void:
+	if not _should_be_moving:
+		return
 
 	if not red_bar:
 		print("Red Bar was found to be null")
@@ -64,9 +66,9 @@ func _move_red_bar(delta: float) -> void:
 	if not _can_move:
 		return
 
-	if red_bar.position.y <= _upper_limits:
+	if red_bar.position.y <= 0.0:
 		_is_moving_down = true
-	elif red_bar.position.y >= _lower_limits:
+	elif red_bar.position.y >= _red_lower_limit:
 		_is_moving_down = false
 
 	if _is_moving_down:
@@ -90,25 +92,63 @@ func _stop_red_bar() -> void:
 	if _is_colliding:
 		print("Success")
 		_choose_new_green_location()
-		_success_boxes[_successes].visible = true
+		_success_boxes[_successes].color.a = 1.0
 		_successes += 1
-		if _successes >= 3:
-			ShipStats.oxygen_amount += stat_success_amount
-			print(ShipStats.oxygen_amount)
-			_successes = 0
-			for b in _success_boxes:
-				b.visible = false
-			on_oxygen_terminal_completion.emit()
+		on_oxygen_terminal_success.emit()
+
+		if _successes < 3:
 			return
 
-		on_oxygen_terminal_success.emit()
+		ShipStats.oxygen_amount += stat_success_amount
+		print(ShipStats.oxygen_amount)
+		_successes = 0
+		for b in _success_boxes:
+			b.color.a = 0.0
+		on_oxygen_terminal_completion.emit()
+		_should_be_moving = false
+
+		for box in _success_boxes:
+			var tween := box.create_tween()
+
+			for i in 3:
+				tween.tween_interval(0.1)
+				tween.tween_property(box, "color:a", 0.0, 0.0)
+				tween.tween_interval(0.1)
+				tween.tween_property(box, "color:a", 1.0, 0.0)
+
+			tween.tween_interval(0.1)
+			tween.tween_property(box, "color:a", 0.0, 0.0)
+			tween.tween_callback(
+				func() -> void:
+					_should_be_moving = true
+			)
+
 	else:
 		print("Fail")
 		_choose_new_green_location()
 		_successes = 0
 		on_oxygen_terminal_failure.emit()
-		for b in _success_boxes:
-			b.visible = false
+
+		for box in _success_boxes:
+			box.color = red
+
+			var tween := box.create_tween()
+
+			for i in 3:
+				tween.tween_interval(0.1)
+				tween.tween_property(box, "color:a", 0.0, 0.0)
+				tween.tween_interval(0.1)
+				tween.tween_property(box, "color:a", 1.0, 0.0)
+
+			tween.tween_interval(0.1)
+			tween.tween_property(box, "color:a", 0.0, 0.0)
+			tween.tween_callback(
+				func() -> void:
+					box.color = green
+					box.color.a = 0.0
+					_should_be_moving = true
+			)
+
 
 func _choose_new_green_location() -> void:
 
@@ -119,10 +159,13 @@ func _choose_new_green_location() -> void:
 	##green_zone.position.y = randf_range(_lower_limits, _upper_limits)
 
 	for i in 10:
-		var rando: float = randf_range(_lower_limits, _upper_limits)
+		var rando: float = randf_range(0.0, _green_lower_limit)
 
-		if abs(rando - green_zone.position.y) > 20:
+		if abs(rando - green_zone.position.y) > 40:
 			green_zone.position.y = rando
 			break
+
+	for arrow in arrows:
+		arrow.position.y = green_zone.position.y + (green_zone.size.y * 0.5) - (arrow.size.y * 0.5)
 
 ## Public Functions
