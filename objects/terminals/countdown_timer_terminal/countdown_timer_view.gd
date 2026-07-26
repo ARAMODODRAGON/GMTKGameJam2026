acfda0c5009d2d51e917d420a8ca029719d0a964
Domara Shlimon <domara2000@icloud.com>
2026-07-25 21:47:14 -0400

    added noise to the countdown terminal

--- objects/terminals/countdown_timer_terminal/countdown_timer_view.gd
+++ objects/terminals/countdown_timer_terminal/countdown_timer_view.gd
@@ -2,10 +2,17 @@ class_name CountdownTimerView
 extends Node2D
 
 @export var timer_text: RichTextLabel
+@export var audioplayer: AudioStreamPlayer3D
+
+var _last_count: int = 0
 
 func _ready() -> void:
 	ShipStats.on_game_timer_amount_changed.connect(update_timer_text)
+	_last_count = int(ShipStats.game_timer)
 
 func update_timer_text(value: float) -> void:
-	
-	timer_text.text = str(int(value))
+	var time := int(value)
+	timer_text.text = str(time)
+	if _last_count != time:
+		_last_count = time
+		audioplayer.play()
